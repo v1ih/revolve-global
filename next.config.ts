082372a41c87b8cf274e2a7b59d2ae82e902a3fa
v1ih <127506581+v1ih@users.noreply.github.com>
2026-09-22 18:14:51 -0300

    Update Next.js configuration for repository settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repo = "revolve-global";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
